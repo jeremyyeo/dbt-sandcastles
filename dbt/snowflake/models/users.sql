43ab9a033d1b4befe992ec835eabac4441870c5a
Jeremy Yeo <jeremy.yeo@dbtlabs.com>
2021-11-08 07:08:23 +0000

    more changes

--- dbt/snowflake/models/users.sql
+++ dbt/snowflake/models/users.sql
@@ -3,5 +3,6 @@ SELECT *
   FROM (VALUES (1, 'active', CONVERT_TIMEZONE('Pacific/Auckland', DATEADD(MINUTE, 35, CURRENT_TIMESTAMP)))
                , (2, 'pending', CONVERT_TIMEZONE('Pacific/Auckland', DATEADD(MINUTE, 45, CURRENT_TIMESTAMP)))
                , (3, 'active', CONVERT_TIMEZONE('Pacific/Auckland', DATEADD(MINUTE, 95, CURRENT_TIMESTAMP)))
+               , (4, 'active', CONVERT_TIMEZONE('Pacific/Auckland', DATEADD(MINUTE, 125, CURRENT_TIMESTAMP)))
        ) 
     AS my_table(user_id, status, updated_at)
