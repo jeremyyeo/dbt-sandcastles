f9e30780bbdca44838aade11f56e6813ee4cb51a
jeremyyeo <jeremyyeo@gmail.com>
2022-01-13 21:01:36 +1300

    update

--- dbt/snowflake/models/my_model.sql
+++ dbt/snowflake/models/my_model.sql
@@ -1,9 +1,1 @@
-with source_data as (
-
-    select 1 as user_id, dateadd(minute, 30, current_timestamp) as updated_at, 'deleted' as status union all
-    select 2, dateadd(minute, 40, current_timestamp), 'active' union all
-    select 3, dateadd(minute, 50, current_timestamp), 'pending'
-
-)
-
-select * from source_data
+select 1 as user
