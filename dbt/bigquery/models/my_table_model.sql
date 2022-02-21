5e8e303ea846ab5aecd8253e961e58e331de2688
jeremyyeo <jeremyyeo@gmail.com>
2022-02-21 15:52:57 +1300

    rename user

--- dbt/bigquery/models/my_table_model.sql
+++ dbt/bigquery/models/my_table_model.sql
@@ -2,4 +2,4 @@
     config(materialized = 'table')
 }}
 
-select 1 as user_id, 'alice' as user_name, current_timestamp() as updated_at
+select 1 as user_id, 'bob' as user_name, current_timestamp() as updated_at
