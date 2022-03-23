3abe83913367efcb3d7258c0bad9e81c0753978b
jeremyyeo <jeremyyeo@gmail.com>
2022-03-23 22:56:29 +1300

    update

--- dbt/snowflake/macros/get_custom_database.sql
+++
@@ -1,7 +0,0 @@
-{% macro generate_database_name(custom_database_name=none, node=none) -%}
-
-    {%- set selected_database = var('db_override', 'development') -%}
-
-    {{ selected_database | trim }}
-
-{%- endmacro %}
