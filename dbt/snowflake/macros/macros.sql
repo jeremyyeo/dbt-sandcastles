439f18d9427cf85772c1094c22c72258bc85c8a5
jeremyyeo <jeremyyeo@gmail.com>
2022-11-25 13:20:06 +1300

    update

--- dbt/snowflake/macros/macros.sql
+++
@@ -1,7 +0,0 @@
-{% macro upload() %}
-    {% do print('Uploading...') %}
-    {% set query %}
-        put file://target/run_results.json @development.dbt_jyeo.flat_files;
-    {% endset %}
-    {% do run_query(query) %}
-{% endmacro %}
