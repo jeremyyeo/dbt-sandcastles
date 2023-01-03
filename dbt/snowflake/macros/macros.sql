9fa9234be5d058e4a4a2b4539578f8b0f7c2d3bc
jeremy.yeo@dbtlabs.com <jeremy.yeo@dbtlabs.com>
2023-01-03 20:31:29 +0000

    make

--- dbt/snowflake/macros/macros.sql
+++ dbt/snowflake/macros/macros.sql
@@ -1,10 +1,4 @@
-{% macro check_result() %}
-  {% set query %}
-    select count(*) as num from (select 1 as id union select 2)
-  {% endset %}
-  {% if execute %}
-    {% set results = run_query(query) %}
-    {% set results_list = 'Number of rows is: ' ~ results.columns[0].values()[0] %}
-    {% do log(results_list, True) %}
-  {% endif %}
+
+{% macro grant_select(this) %}
+    -- no-op
 {% endmacro %}
--- dbt/snowflake/models/foo.sql
+++ dbt/snowflake/models/foo.sql
@@ -1,2 +1,7 @@
+{{
+    config(
+        grants = {'select': ['dbt_' + target.name + '_role', 'dbt_' + target.name + '_role']}
+    )
+}}
 
-select * from {{ source('dbt_jyeo', 'my_source_foo') }}
+select 1 as id
