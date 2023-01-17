7609ab84ed5bbd6c56326934a55b19d8283c3e1a
jeremyyeo <jeremyyeo@gmail.com>
2023-01-18 12:42:56 +1300

    update

--- dbt/snowflake/macros/check_upstreams.sql
+++
@@ -1,7 +0,0 @@
-{% macro get_model_dependencies() %}
-    {% if execute %}
-        {% for node in graph.nodes.values() | selectattr("resource_type", "equalto", "model") %}
-            {% do print(node.unique_id ~ ' upstream models are: ' ~ node.depends_on.nodes) %}
-        {% endfor %}
-    {% endif %}
-{% endmacro %}
---
+++ dbt/snowflake/models/orders.sql
@@ -0,0 +1,8 @@
+
+select 1 as id, 1 as amount, '1970-01-01'::date as ts
+union
+select 2 as id, 2 as amount, '1970-01-01'::date as ts
+union
+select 3 as id, 3 as amount, '1970-01-02'::date as ts
+union
+select 4 as id, 4 as amount, '1970-01-02'::date as ts
