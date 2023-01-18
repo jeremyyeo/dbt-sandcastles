2815886350d7364e117de79266f340c4f3964f94
Jeremy Yeo <jeremy.yeo@dbtlabs.com>
2023-01-18 00:02:04 +0000

    update

--- dbt/snowflake/models/orders.sql
+++ dbt/snowflake/models/orders.sql
@@ -1,8 +1,8 @@
 
-select 1 as id, 1 as amount, '1970-01-01'::date as ts
+select 1 as id, 1 as amount, '2000-01-01'::date as ts
 union
-select 2 as id, 2 as amount, '1970-01-01'::date as ts
+select 2 as id, 2 as amount, '2000-01-01'::date as ts
 union
-select 3 as id, 3 as amount, '1970-01-02'::date as ts
+select 3 as id, 3 as amount, '2000-01-02'::date as ts
 union
-select 4 as id, 4 as amount, '1970-01-02'::date as ts
+select 4 as id, 4 as amount, '2000-01-02'::date as ts
